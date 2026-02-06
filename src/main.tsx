40c2bcf1e8cabc206a6b00436e5ac0cf31496271
erikknoos <erikknoos@gmail.com>
2026-02-07 00:48:28 +0100

    removed import

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import LoginModal from './LoginModal';
+
 
 interface CO2FormData {
   companyName: string;
